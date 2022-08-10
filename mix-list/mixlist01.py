2e702fac7377b1391111a6c3addf12f8c4bab947
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-10 15:47:05 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("The first item in the list (IP): " + my_list[0])
+
+print("The second item in the list (PORT): " + str(my_list[1]) )
+
+
+print("The last item in the list (state): " + my_list[2] )
+
+print("the IP addresses are:" + iplist[3] + ", and " +  iplist[4])
+
+

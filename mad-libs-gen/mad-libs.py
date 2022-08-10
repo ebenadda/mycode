cbc3865e844b47acecb0701b4cb64ff8af0e18ad
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-10 22:40:21 +0000

    intial commit for Mad Libs Generator

---
+++ mad-libs-gen/mad-libs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''Mad Libs Generator
+
+This code allows you to respond to some questions and prints out senteneces based on your response'''
+
+#Loop back to this point once the code finishes
+
+loop = 1
+
+while (loop <10):
+
+    #All the questions the program asks user
+
+    noun = input ('choose a noun: ')
+
+
+
+#Loop back to "loop = 1"
+    loop = loop + 1

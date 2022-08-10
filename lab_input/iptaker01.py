e1685b8294e7be70bc98ec35ea05ea8875c84194
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-10 15:04:32 +0000

    learning about input function

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""ALta3 Research |RZFeeser
+print() - display data to std out"""
+
+#below is a function containing our code
+def main():
+
+    #pause the program and waint for the user to provide input
+    user_input = input("Please enter an IPv4 IP address:")
+
+    # display the input back to the user.
+    print("You told me the IPv4 address is: " + user_input)
+
+# this calls main
+main()

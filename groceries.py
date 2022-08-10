66fe45f0936b97fb8435ff14100d09928c01ee34
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-10 15:31:52 +0000

    groceries list example

---
+++ groceries.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+friedChicken = ["Chicken", "Flour", "Cornstarch", "Eggs", "Buttermilk"]
+seasoningBlend = ["paprika", "old bay", "garlic", "onion powder", "oregeno", "salt", "pepper"]
+
+groceries = friedChicken + seasoningBlend
+
+print(friedChicken[2])
+
+print(friedChicken)
+
+print(seasoningBlend)
+
+print(groceries)
+

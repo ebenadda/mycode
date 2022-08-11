1ddc5dda47720bf0676e1635cce8e7dff7552854
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-11 14:34:02 +0000

    added more lines to mad-libs-gen

--- mad-libs-gen/mad-libs.py
+++ mad-libs-gen/mad-libs.py
@@ -12,8 +12,11 @@ while (loop <10):
 
     #All the questions the program asks user
 
-    noun = input ('choose a noun: ')
-
+    noun = input('choose a noun: ')
+    p_noun = input('Choose a plural noun: ')
+    noun2 =  input('choose a noun: ')
+    place = input('name a place: ')
+    adjective = input(
 
 
 #Loop back to "loop = 1"

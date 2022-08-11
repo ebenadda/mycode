0fa4f74bca70340017d1d5417de9456f4fc6a016
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-11 21:25:57 +0000

    studying for logic

---
+++ introfor/forloop3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RZFeeser | Alta3 Research
+   learning about for logic"""
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+
+for x in farms:
+       print(x)

971fb5d6c7b087606ab5f662dc3e80961c3a6046
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-15 17:29:54 +0000

    Pylint formating

---
+++ smallscript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Alta3 | Ebenezer Addae
+The purpose is to learn how to use pylint for formating our code'''
+
+def main():
+    """Our main program"""
+    my_note = "print this string" # define mynote
+    print(my_note) # display our string
+
+main()

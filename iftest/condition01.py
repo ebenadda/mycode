e1eb39518a110d7cc2cd048dce39656a1901ef80
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-15 19:12:18 +0000

    Testing with Pytest

--- iftest/condition01.py
+++ iftest/condition01.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+'''Alta3 | Ebenezer Addae
+    I am editing this to conform with pylint style guide'''
 
 # create the string hostname
-hostname = "MTG"
+HOST_NAME = "MTG"
 
 # test logic with the `if` statement
 # what to do if this statement is found to be true
-if hostname == "MTG":
+if HOST_NAME == "MTG":
     print("The hostname was found to be MTG")
-
-
--- iftest/condition02.py
+++ iftest/condition02.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+'''Alta3 | Ebenezer Addae
+    This document is being formated to conform with pylint style guide'''
 
 hostname = input("What value should we set for hostname?")
 
 if hostname == "MTG":
     print("The hostname was found to be MTG")
-

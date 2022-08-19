859a5d2484678a08184430c112085fcdc46efeea
Ebenezer Addae <ebenadda@users.noreply.github.com>
2022-08-19 10:35:08 +0000

    added xaxis index

--- project_health_app/graph.py
+++ project_health_app/graph.py
@@ -10,21 +10,18 @@ def graphin():
     plt.rcParams["figure.figsize"] = [7.00, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
-    # Make a list of columns
-    columns = ['bmi']
-    
     # Read a CSV file
-    df = pd.read_csv("bmi_data3.csv", usecols=columns)
+    df = pd.read_csv("bmi_data3.csv")
 
     # Plot the line graph with the specified labels
-    df.plot()
+    df.plot(x='date',y='bmi',xticks=df.index)
     plt.title('BMI Change Over Time')
     plt.xlabel('Date')
     plt.ylabel('BMI kg/m\u00b2')
     #plt.show()
     
     #Save the graph to a desired location
-    plt.savefig('/home/student/static/bmi_graph7.png')
+    plt.savefig('/home/student/static/bmi_graph88888888.png')
     #plt.savefig('/home/student/mycode/project_health_app/bmi_graph4.png')
     
 graphin()
